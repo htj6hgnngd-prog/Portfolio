852c701c16f0146dfcd16cc3dbdbc9189616497b
htj6hgnngd-prog <reshaet@icloud.com>
2026-09-18 01:26:46 +0300

    Bust photo gallery cache after event reorder

--- event-video.js
+++ event-video.js
@@ -52,6 +52,6 @@
   });
 })();
 
-import("/photo-gallery.js?v=3").catch((error) => {
+import("/photo-gallery.js?v=4").catch((error) => {
   console.error("Photo gallery failed to load", error);
 });
