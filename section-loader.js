8529b466f7f2640cdb186755594f25414b8803e1
htj6hgnngd-prog <reshaet@icloud.com>
2026-09-18 05:05:11 +0300

    Bust showcase caches after animation rebuild

--- section-loader.js
+++ section-loader.js
@@ -40,11 +40,11 @@
 
   near("photo", 1200, () => import("/photo-gallery.js?v=6"));
   near("product", 1900, async () => {
-    await loadStyle("/product-experience.css?v=1");
-    await import("/product-experience.js?v=1");
+    await loadStyle("/product-experience.css?v=2");
+    await import("/product-experience.js?v=2");
   });
   near("site", 2100, async () => {
-    await loadStyle("/site-experience.css?v=1");
-    await import("/site-experience.js?v=1");
+    await loadStyle("/site-experience.css?v=2");
+    await import("/site-experience.js?v=2");
   });
 })();
